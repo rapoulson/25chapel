ce752cf551d4fa67671924f7a562c25747c5907b
rapoulson <rebecca.poulson@gmail.com>
2014-05-29 17:00:52 -0400

    put mouse down

--- lib/world.rb
+++ lib/world.rb
@@ -7,10 +7,52 @@ root = Node.root do
 		self.exit_east = :hall
 
 		item(:cat, 'cat', 'sleeping', 'fuzzy') do
+			self.script_take = <<-SCRIPT
+			if find(:dead_mouse)
+				puts "The cat drops the dead mouse"
+				get_room.move(:dead_mouse, get_room, false)
+			end
+
+			puts "The can't won't let you pick it up"
+			return false
+			SCRIPT
+			self.script_control = <<-SCRIPT
+			puts "The cat sits upright, awaiting your command)"
+				return true
+			SCRIPT
+
 			item(:dead_mouse, 'mouse', 'dead', 'eaten')
 		end
 
-		item(:remote_control, 'remote', 'control') do 
+		item(:remote_control, 'remote', 'control') do
+		self.script_accept = <<-SCRIPT
+			if [:new_batteries, :old_batteries].include?(args[0].tag) &&
+				children.empty?
+				return true
+			elsif !children.empty?
+				puts "There are already batteries in the remote"
+				return false
+			else
+				puts "That won't fit into the remote"
+				return false
+			end
+			SCRIPT
+
+			self.script_use = <<-SCRIPT
+			if !find(:new_batteries)
+				puts "The remote doesn't seem to work"
+				return
+			end
+
+			if args[0].tag == :cat
+				args[0].script('control')
+				return
+			else
+				puts "The remote doesn't seem to work with that"
+				return
+			end
+			SCRIPT
+
 			item(:dead_batteries, 'batteries', 'dead', 'AA')
 		end
 	end
@@ -31,6 +73,11 @@ root = Node.root do
 
 	room(:hall) do
 		self.exit_west = :living_room
+
+		self.script_enter = <<-SCRIPT
+		puts "A forcefield stops you from entering the hall"
+		return false
+		SCRIPT
 	end
 end
 
